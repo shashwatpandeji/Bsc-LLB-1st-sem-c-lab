df1f49cdf8415efed77db86c2b2c412ac21118aa
shashwat pande <shashwatpande9d@gmail.com>
2025-08-28 11:31:12 +0530

    Add files via upload

---
+++ find area  and perimeter of rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main ()
+{
+    float length, width, area, permimeter;
+    printf ("enter the length of the rectangle:");
+    scanf ("%f", & length);
+    printf ("enter the width of the rectangle:");
+    scanf ("%f", & width);
+     area = length * width;
+     permimeter = 2 * (length + width);
+     printf ("area of the rectangle = %f\n", area);
+     printf ("perimeter of the rectangle = %f\n", permimeter);
+}
