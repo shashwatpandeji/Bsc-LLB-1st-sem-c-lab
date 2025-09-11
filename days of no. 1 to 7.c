5adcedd7d916b822e2f850880fda630e68632ecc
shashwat pande <shashwatpande9d@gmail.com>
2025-09-11 12:22:06 +0530

    Add files via upload

---
+++ days of no. 1 to 7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main() {
+    int choice;
+
+    printf("Enter a number (1-7): ");
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 1:
+            printf("Sunday\n");
+            break;
+        case 2:
+            printf("Monday\n");
+            break;
+        case 3:
+            printf("Tuesday\n");
+            break;
+        case 4:
+            printf("Wednesday\n");
+            break;
+        case 5:
+            printf("Thursday\n");
+            break;
+        case 6:
+            printf("Friday\n");
+            break;
+        case 7:
+            printf("Saturday\n");
+            break;
+        default:
+            printf("wrong choice.\n");
+    }
+
+}
