2d9b4e5fee61e8d4465a922ea38ac5bf26bab6ee
shashwat pande <shashwatpande9d@gmail.com>
2025-09-11 12:32:09 +0530

    Add files via upload

---
+++ no is divisible by 3 and 5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 3 == 0) {
+        if (num % 5 == 0) {
+            printf("%d is divisible by both 3 and 5.\n", num);
+        } else {
+            printf("%d is divisible by 3 but not by 5.\n", num);
+        }
+    } else {
+        if (num % 5 == 0) {
+            printf("%d is divisible by 5 but not by 3.\n", num);
+        } else {
+            printf("%d is not divisible by 3 or 5.\n", num);
+        }
+    }
+}
